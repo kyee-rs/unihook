d9add0177d73da94e7ecbbe5c921e69a57712b0a
Jenny Poulson <911us@duck.com>
2022-12-03 21:25:55 +0000

    Final stage

--- api/bot.ts
+++ api/bot.ts
@@ -1,11 +1,24 @@
+import { createHash } from "crypto";
 import { Bot } from "grammy";
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 
-bot.command("start", (ctx) =>
-    ctx.reply(
-        "Hello, I am a GitGuardian helper bot! I can help you to protect your GitHub repositories from secrets leaks. To get started, please visit https://gitguardian.com",
-    ),
-);
+bot.command("start", async (ctx) => {
+    await ctx.reply(
+        "Hello, I am a GitGuardian helper bot! I can help you to protect your GitHub repositories from secrets leaks. To get started, please follow the instructions: \n" +
+            "1. Go to https://gitguardian.com/ and sign up.\n" +
+            "2. Go to https://dashboard.gitguardian.com/integrations and create a new webhook integration.\n" +
+            "3. Copy the webhook URL from next message and paste it in the field below.\n" +
+            "4. Save and send test message.",
+    );
+    await ctx.reply(
+        "https://gg.ieljit.lol/webhook/" +
+            ctx.from?.id +
+            "/" +
+            createHash("sha256")
+                .update(ctx.from?.id + process.env.WEBHOOK_SECRET!)
+                .digest("hex"),
+    );
+});
 
 export default bot;
--- api/webhooks.ts
+++ api/webhooks.ts
@@ -1,13 +1,22 @@
 import { json } from "body-parser";
+import { createHash } from "crypto";
 import express from "express";
 import { webhookCallback } from "grammy";
 import bot from "./bot";
-
 const app = express();
 app.use(json());
 
 app.post(`/${process.env.BOT_TOKEN}`, webhookCallback(bot, "express"));
-app.post("/webhook/:token", (req, res) => {
+app.post("/webhook/:id/:token", (req, res) => {
+    if (!req.params.id) return res.status(400).send("Missing token");
+    if (isNaN(<any>req.params.id)) return res.status(400).send("Invalid token");
+    if (
+        req.params.token !==
+        createHash("sha256")
+            .update(req.params.id + process.env.WEBHOOK_SECRET)
+            .digest("hex")
+    )
+        return res.status(400).send("Invalid token");
     const data: {
         author: { info: string; name: string };
         date: Date;
@@ -23,15 +32,14 @@ app.post("/webhook/:token", (req, res) => {
         }[];
     } = req.body;
     bot.api.sendMessage(
-        req.params.token,
-        `📢 *New alert from GitGuardian*:\nAuthor: \`${data.author.name}<${data.author.info}>\`\nDate: \`${new Date(
+        req.params.id,
+        `📢 *New alert from GitGuardian*:\nAuthor: \`${data.author.name} <${data.author.info}>\`\nDate: \`${new Date(
             data.date,
-        ).toDateString()}\`\nType: \`${data.type}\`\nPolicy: \`${data.policy}\`\nGitGuardian link: \`${
+        ).toDateString()}\`\nType: \`${data.type}\`\nPolicy: \`${data.policy}\`\nGitGuardian link: [GG Link](${
             data.gitguardian_link
-        }\`\nBreak URL: \`${data.break_url}\`\nValidity: \`${data.validity}\``,
+        })\nBreak URL: [File link](${data.break_url})\nValidity: \`${data.validity}\``,
         { parse_mode: "Markdown" },
     );
     res.send("ok");
 });
 export = app;
-// TEST TOKEN: 5718541363:AAExrzi1bumzgPIdZK1LohO089tqsyweZqI
