9b1b07d3f27bc663930d1235f7edca140df07785
Jenny Poulson <911us@duck.com>
2022-12-04 17:02:41 +0000

    universal webhook handler

--- api/bot.ts
+++ api/bot.ts
@@ -1,24 +1,100 @@
+import { Conversation, ConversationFlavor, conversations, createConversation } from "@grammyjs/conversations";
+import { PrismaClient } from "@prisma/client";
 import { createHash } from "crypto";
-import { Bot } from "grammy";
+import { Bot, Context, session } from "grammy";
+const database = new PrismaClient();
+type MyContext = Context & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
 
-const bot = new Bot(process.env.BOT_TOKEN!);
+async function set(conversation: MyConversation, ctx: MyContext) {
+    if (!(await database.user.findUnique({ where: { id: ctx.from?.id } }))) {
+        return await ctx.reply("You are not registered. Please use /start to register.");
+    }
 
-bot.command("start", async (ctx) => {
     await ctx.reply(
-        "Hello, I am a GitGuardian helper bot! I can help you to protect your GitHub repositories from secrets leaks. To get started, please follow the instructions: \n" +
-            "1. Go to https://gitguardian.com/ and sign up.\n" +
-            "2. Go to https://dashboard.gitguardian.com/integrations and create a new webhook integration.\n" +
-            "3. Copy the webhook URL from next message and paste it in the field below.\n" +
-            "4. Save and send test message.",
+        "Please send me the pattern you want to add. Use /cancel to cancel.\n\nExample: `New alert from Webhook {data.webhook_name}`",
     );
+    const pattern = await conversation.waitFor(":text");
+    if (!pattern.message?.text) return;
+    await ctx.reply("Please send me the webhook ID you want to add this pattern to. \n\nExample: `my-webhook`");
+    const id = await conversation.waitFor(":text");
+    if (!id.message?.text) return;
+    if (await database.pattern.findUnique({ where: { id: id.message?.text } })) {
+        return await ctx.reply("This webhook already exists.");
+    }
+    await database.pattern.create({
+        data: {
+            pattern: pattern.message.text,
+            id: id.message.text,
+            User: {
+                connect: {
+                    id: ctx.from?.id,
+                },
+            },
+        },
+    });
+    await database.$disconnect();
+    await ctx.reply("Pattern added! You can now use /list to see all your patterns.");
     await ctx.reply(
-        "https://gg.ieljit.lol/webhook/" +
+        "URL: " +
+            "http://localhost:3000/" +
             ctx.from?.id +
             "/" +
             createHash("sha256")
                 .update(ctx.from?.id + process.env.WEBHOOK_SECRET!)
-                .digest("hex"),
+                .digest("hex") +
+            "/" +
+            id.message?.text,
     );
+}
+
+export const bot = new Bot<MyContext>(process.env.BOT_TOKEN!);
+
+bot.use(
+    session({
+        initial() {
+            return {};
+        },
+    }),
+);
+
+bot.use(conversations());
+bot.use(createConversation(set));
+
+bot.command("start", async (ctx) => {
+    // await ctx.reply(
+    //     "Hello, I am a GitGuardian helper bot! I can help you to protect your GitHub repositories from secrets leaks. To get started, please follow the instructions: \n" +
+    //         "1. Go to https://gitguardian.com/ and sign up.\n" +
+    //         "2. Go to https://dashboard.gitguardian.com/integrations and create a new webhook integration.\n" +
+    //         "3. Copy the webhook URL from next message and paste it in the field below.\n" +
+    //         "4. Save and send test message.",
+    // );
+    // await ctx.reply(
+    //     "https://hook.ieljit.lol/webhook/" +
+    //         ctx.from?.id +
+    //         "/" +
+    //         createHash("sha256")
+    //             .update(ctx.from?.id + process.env.WEBHOOK_SECRET!)
+    //             .digest("hex"),
+    // );
+    await ctx.reply("Registering...");
+    if (await database.user.findUnique({ where: { id: ctx.from?.id } })) {
+        return await ctx.reply("You are already registered.");
+    }
+    await database.user.create({
+        data: {
+            id: ctx.from!.id,
+        },
+    });
+    await database.$disconnect();
+    await ctx.reply("Registered!");
 });
 
-export default bot;
+bot.command("set", async (ctx) => {
+    await ctx.conversation.enter("set");
+});
+
+bot.command("cancel", async (ctx) => {
+    await ctx.conversation.exit();
+    await ctx.reply("Canceled.");
+});
--- api/webhooks.ts
+++ api/webhooks.ts
@@ -1,15 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 import { json } from "body-parser";
 import { createHash } from "crypto";
 import express from "express";
-import { webhookCallback } from "grammy";
-import bot from "./bot";
+// import { webhookCallback } from "grammy";
+import { bot } from "./bot";
 const app = express();
+const database = new PrismaClient();
 app.use(json());
 
-app.post(`/${process.env.BOT_TOKEN}`, webhookCallback(bot, "express"));
-app.post("/webhook/:id/:token", (req, res) => {
-    if (!req.params.id) return res.status(400).send("Missing token");
+// app.post(`/${process.env.BOT_TOKEN}`, webhookCallback(bot, "express"));
+app.post("/:id/:token/:hookid", (req, res) => {
+    if (!req.params.id) return res.status(400).send("Missing id");
     if (isNaN(<any>req.params.id)) return res.status(400).send("Invalid token");
+    if (!req.params.token) return res.status(400).send("Missing token");
+    if (!req.params.hookid) return res.status(400).send("Missing hookid");
     if (
         req.params.token !==
         createHash("sha256")
@@ -17,29 +21,38 @@ app.post("/webhook/:id/:token", (req, res) => {
             .digest("hex")
     )
         return res.status(400).send("Invalid token");
-    const data: {
-        author: { info: string; name: string };
-        date: Date;
-        type: string;
-        policy: string;
-        gitguardian_link: string;
-        break_url: string;
-        severity: string;
-        validity: string;
-        matches: {
-            type: string;
-            match: string;
-        }[];
-    } = req.body;
-    bot.api.sendMessage(
-        req.params.id,
-        `📢 *New alert from GitGuardian*:\nAuthor: \`${data.author.name} <${data.author.info}>\`\nDate: \`${new Date(
-            data.date,
-        ).toDateString()}\`\nType: \`${data.type}\`\nPolicy: \`${data.policy}\`\nGitGuardian link: [GG Link](${
-            data.gitguardian_link
-        })\nBreak URL: [File link](${data.break_url})\nValidity: \`${data.validity}\``,
-        { parse_mode: "Markdown" },
-    );
-    res.send("ok");
+    const data = req.body;
+    if (!data) return res.status(400).send("Missing payload");
+    database.user.findUnique({ where: { id: parseInt(req.params.id) } }).then((user) => {
+        if (!user) return res.status(400).send("Invalid user");
+        database.pattern
+            .findUnique({ where: { id: req.params.hookid } })
+            .then((webhook) => {
+                if (!webhook) return res.status(400).send("Invalid webhook");
+                let message = webhook.pattern;
+                if (!message) return res.status(400).send("Invalid webhook");
+                try {
+                    const regex = /{([^}]+)}/g;
+                    const matches = message.match(regex);
+                    if (matches) {
+                        for (const match of matches) {
+                            const key = match.replace("{", "").replace("}", "");
+                            const value = data[key] || "<undefined_value>";
+                            message = message.replace(match, value);
+                        }
+                    }
+                    bot.api.sendMessage(req.params.id, message);
+                    res.send("ok");
+                } catch (e) {
+                    res.status(500).send("Internal server error");
+                }
+            })
+            .then(() => database.$disconnect())
+            .catch(() => {
+                res.status(500).send("Internal server error");
+                database.$disconnect();
+            });
+    });
 });
-export = app;
+bot.start();
+app.listen(3000);
